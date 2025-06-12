290387949ed06cb4bda5777d2139a99c42ecc6a7
Edoka Isaac <edokaizkdaking247@gmail.com>
2025-06-12 23:00:49 +0100

    Setup Library Contract

---
+++ src/Library.sol
@@ -0,0 +1,111 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.29;
+
+contract Library {
+    address public Librarian;
+    uint256 public nextBookId;
+
+    error NotLibrarian(string reason);
+    error InvalidCopies(string reason);
+    error BookDoesNotExist(string reason);
+    error NoAvailableCopies(string reason);
+    error AlreadyBorrowed(string reason);
+    error NotBorrowedYet(string reason);
+
+    struct Book {
+        uint256 id;
+        string title;
+        string author;
+        uint256 copies;
+    }
+
+    mapping(uint256 => Book) public books;
+    mapping(address => mapping(uint256 => bool)) public hasBorrowed;
+    mapping(address => uint256[]) private borrowedBooks;
+
+    event BookAdded(uint256 indexed ID, string title, string author, uint256 copies);
+    event BookBorrowed(address indexed user, uint256 indexed ID);
+    event BookReturned(address indexed user, uint256 indexed ID);
+
+    constructor() {
+        Librarian = msg.sender;
+    }
+
+     modifier OnlyLibrarian() {
+        if (msg.sender != Librarian) revert NotLibrarian("Only The Librarian Can Call This Function");
+        _;
+    }
+
+    modifier bookExists(uint256 ID) {
+        if (ID >= nextBookId) revert BookDoesNotExist("Book Not Found");
+        _;
+    }
+
+    function addBook(string calldata _title, string calldata _author, uint256 _copies) external OnlyLibrarian {
+        if (_copies == 0) revert InvalidCopies("Must Have At Least 1 Copy");
+
+        books[nextBookId] = Book({
+            id: nextBookId,
+            title: _title,
+            author: _author,
+            copies: _copies
+        });
+
+        emit BookAdded(nextBookId, _title, _author, _copies);
+
+        nextBookId++;
+    }
+
+    function borrowBook(uint256 ID) external bookExists(ID) {
+        Book storage book = books[ID];
+
+        if (book.copies == 0) revert NoAvailableCopies("No Copies Available To Borrow");
+        if (hasBorrowed[msg.sender][ID]) revert AlreadyBorrowed("You Already Borrowed This Book");
+
+        book.copies--;
+        hasBorrowed[msg.sender][ID] = true;
+        borrowedBooks[msg.sender].push(ID);
+
+        emit BookBorrowed(msg.sender, ID);
+    }
+
+    function returnBook(uint256 ID) external bookExists(ID) {
+        if (!hasBorrowed[msg.sender][ID]) revert NotBorrowedYet("You Haven't Borrowed This Book");
+
+        Book storage book = books[ID];
+        book.copies++;
+        hasBorrowed[msg.sender][ID] = false;
+
+        uint256[] storage borrowed = borrowedBooks[msg.sender];
+
+        for (uint256 i = 0; i < borrowed.length; i++) {
+            if (borrowed[i] == ID) {
+                borrowed[i] = borrowed[borrowed.length - 1];
+                borrowed.pop();
+                break;
+            }
+        }
+
+        emit BookReturned(msg.sender, ID);
+    }
+
+    function getBook(uint256 ID) external view bookExists(ID) returns (Book memory) {
+        return books[ID];
+    }
+
+    function getAvailableCopies(uint256 ID) external view bookExists(ID) returns (uint256) {
+        return books[ID].copies;
+    }
+
+    function getAllBooks() external view returns (Book[] memory) {
+        Book[] memory all = new Book[](nextBookId);
+        for (uint256 i = 0; i < nextBookId; i++) {
+            all[i] = books[i];
+        }
+        return all;
+    }
+
+    function getUserBorrowedBooks(address user) external view returns (uint256[] memory) {
+        return borrowedBooks[user];
+    }
+}
---
+++ test/Library.t.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.29;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Library} from "../src/Library.sol";
+
+contract LibraryTestContract is Test {
+    Library Lib;
+    address librarian;
+    address user1;
+    address user2;
+
+    function setUp() public {
+        Lib = new Library();
+        librarian = address(this);
+        user1 = address(0x123);
+        user2 = address(0x124);
+    }
+
+    function testAddBook() public {
+        vm.prank(librarian);
+        Lib.addBook("The White Wizard", "Tade Adegbindin", 5);
+
+        (,, string memory author, uint256 copies) = Lib.books(0);
+        assertEq(author, "Tade Adegbindin");
+        assertEq(copies, 5);
+    }
+
+    function testUserAddBook() public {
+        vm.prank(user1);
+        vm.expectRevert(abi.encodeWithSelector(Library.NotLibrarian.selector, "Only The Librarian Can Call This Function"));
+        Lib.addBook("Htiler: The Legend", "Emmanuel Nzuebe", 1);
+    }
+
+    function testAddBookZeroCopies() public {
+        vm.expectRevert(abi.encodeWithSelector(Library.InvalidCopies.selector, "Must Have At Least 1 Copy"));
+        Lib.addBook("Animal Farm", "George Orwell", 0);
+    }
+
+    function testBorrowAndReturnBook() public {
+        Lib.addBook("The Art of War", "Sun Tzu", 3);
+
+        vm.prank(user1);
+        Lib.borrowBook(0);
+        uint available = Lib.getAvailableCopies(0);
+        assertEq(available, 2);
+
+        vm.prank(user1);
+        Lib.returnBook(0);
+        available = Lib.getAvailableCopies(0);
+        assertEq(available, 3);
+    }
+
+    function testBorrowBookTwice() public {
+        Lib.addBook("The White Wizard", "Tade Adegbindin", 5);
+
+        vm.prank(user1);
+        Lib.borrowBook(0);
+
+        vm.prank(user1);
+        vm.expectRevert(abi.encodeWithSelector(Library.AlreadyBorrowed.selector, "You Already Borrowed This Book"));
+        Lib.borrowBook(0);
+    }
+
+    function testReturnBookWithoutBorrowing() public {
+        Lib.addBook("The Subtle Art of Not Giving a F*ck", "Mark Manson", 1);
+
+        vm.prank(user1);
+        vm.expectRevert(abi.encodeWithSelector(Library.NotBorrowedYet.selector, "You Haven't Borrowed This Book"));
+        Lib.returnBook(0);
+    }
+
+    function testGetUserBorrowedBooks() public {
+        Lib.addBook("Animal Farm", "George Orwell", 5);
+        Lib.addBook("Htiler: The Legend", "Emmanuel Nzuebe", 30);
+        Lib.addBook("The Subtle Art of Not Giving a F*ck", "Mark Manson", 1);
+
+
+        vm.prank(user1);
+        Lib.borrowBook(0);
+
+        vm.prank(user1);
+        Lib.borrowBook(1);
+
+        vm.prank(user1);
+        Lib.borrowBook(2);
+
+        uint256[] memory borrowed = Lib.getUserBorrowedBooks(user1);
+        assertEq(borrowed.length, 3);
+    }
+}
